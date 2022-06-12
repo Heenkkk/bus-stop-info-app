161cf24ac459720e6bdeff5cb67ed94f487a4a5e
Heenkkk <henrisodergard@gmail.com>
2022-06-12 12:18:59 +0300

    Add initial input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    searchText.length > 0 && console.log(searchText);
+  }, [searchText]);
+
   return (
-    <div>
-      <h1>React App</h1>
+    <div className="content">
+      <h1 style={{ textAlign: 'center' }}>React App</h1>
+      <input
+        type="text"
+        name="search"
+        value={searchText}
+        onChange={(event) => setSearchText(event.target.value)}
+        style={{ display: 'block', margin: '0 auto' }}
+      />
     </div>
   );
 };
